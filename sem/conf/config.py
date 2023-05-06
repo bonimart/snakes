6eb662e254ff7d1cabb3dc0f17a2355ba2ea868a
Martin Benes <benesm44@fit.cvut.cz>
2023-05-06 16:00:04 +0200

    add basic config for the game

---
+++ sem/conf/config.py
@@ -0,0 +1,13 @@
+from catppuccin import Flavour
+
+BLOCK_SIZE = 40
+HEIGHT = 20
+WIDTH = 30
+WIN_HEIGHT = BLOCK_SIZE*HEIGHT
+WIN_WIDTH = BLOCK_SIZE*WIDTH
+
+PALETTE = Flavour.mocha()
+BACKGROUND_COLOR = PALETTE.crust.rgb()
+SNAKE_COLOR = PALETTE.green.rgb()
+FRUIT_COLOR = PALETTE.yellow.rgb()
+
