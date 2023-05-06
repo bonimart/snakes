cfaa6a42c97280c5d9fdc528e81de7a8e4c3d168
Martin Benes <benesm44@fit.cvut.cz>
2023-05-06 17:02:14 +0200

    sem: add background rectangle for Map

--- sem/src/game/Map.py
+++ sem/src/game/Map.py
@@ -1,6 +1,10 @@
 from pyglet.math import Vec2
+from pyglet.graphics import Batch
+from pyglet.shapes import Rectangle
+from conf.config import BACKGROUND_COLOR, BLOCK_SIZE, BLOCK_SIZE
 
 class Map:
-    def __init__(self, shape: Vec2):
+    def __init__(self, shape: Vec2, batch: Batch):
         self.shape = shape
-
+        self.sprite = Rectangle(0, 0, BLOCK_SIZE*shape.x, BLOCK_SIZE*shape.y, BACKGROUND_COLOR, batch=batch)
+        
