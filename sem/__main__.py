43ddd4fe973a681e1116da20c46088b60d9bd9a1
Martin Benes <benesm44@fit.cvut.cz>
2023-05-06 18:49:59 +0200

    sem: add controls for the snake for testing purposers

--- sem/__main__.py
+++ sem/__main__.py
@@ -2,6 +2,7 @@ import pyglet
 from pyglet.window import key
 from conf.config import WIN_WIDTH, WIN_HEIGHT
 from src.game.Game import Game
+from random import choice
 
 window = pyglet.window.Window(WIN_WIDTH, WIN_HEIGHT)
 game = Game()
@@ -11,10 +12,22 @@ def on_draw():
     window.clear()
     game.draw()
 
-# @window.event
-# def on_key_press(symbol, modifiers):
-#     if symbol == key.SPACE:
-#         game.fruit.respawn()
+def update(dt):
+    game.step()
+
+pyglet.clock.schedule_interval(update, 1/10)
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.UP:
+        game.dir = "u"
+    elif symbol == key.DOWN:
+        game.dir = "d"
+    elif symbol == key.LEFT:
+        game.dir = "l"
+    elif symbol == key.RIGHT:
+        game.dir = "r"
+
 
 if __name__ == '__main__':
     pyglet.app.run()
